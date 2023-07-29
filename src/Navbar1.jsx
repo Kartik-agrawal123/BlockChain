73cbfdbdc57c7df43a78456518568340592a1a8b
Kartik Agrawal <kartikagrawal4321@gmail.com>
2023-07-29 12:41:33 +0530

    navbar change

--- src/Navbar1.jsx
+++ src/Navbar1.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar1 = () => {
 
@@ -20,16 +21,16 @@ const Navbar1 = () => {
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border  rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
         <li>
-          <a href="/" class="block py-2 pl-3 pr-4 text-white  rounded md:bg-transparent md:p-0 dark:text-white md:hover:text-blue-700 " aria-current="page">Home</a>
+          <NavLink to="/" class="block py-2 pl-3 pr-4 text-white  rounded md:bg-transparent md:p-0 dark:text-white md:hover:text-blue-700 " aria-current="page">Home</NavLink>
         </li>
         <li>
-          <a href="/coins" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500   dark:hover:text-white md:dark:hover:bg-transparent">Coins</a>
+          <NavLink to="/coins" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white md:dark:hover:text-blue-500   dark:hover:text-white md:dark:hover:bg-transparent">Coins</NavLink>
         </li>
         <li>
-          <a href="/exchange" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Exchange</a>
+          <NavLink to="/exchange" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Exchange</NavLink>
         </li>
         <li>
-          <a href="/blog" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</a>
+          <NavLink to="/blog" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</NavLink>
         </li>
         
       </ul>
